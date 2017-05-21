38dda39a6eb192219c2bb68fa50af986257fb599
Python3pkg <raliclo@gmail.com>
2017-05-21 06:17:36 -0700

    Convert to Python3

--- docs/conf.py
+++ docs/conf.py
@@ -49,9 +49,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Eliza'
-copyright = u'2016, Jens Schaa'
-author = u'Jens Schaa'
+project = 'Eliza'
+copyright = '2016, Jens Schaa'
+author = 'Jens Schaa'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -232,8 +232,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'Eliza.tex', u'Eliza Documentation',
-   u'Jens Schaa', 'manual'),
+  (master_doc, 'Eliza.tex', 'Eliza Documentation',
+   'Jens Schaa', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -262,7 +262,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'Eliza', u'Eliza Documentation',
+    (master_doc, 'Eliza', 'Eliza Documentation',
      [author], 1)
 ]
 
@@ -276,7 +276,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'Eliza', u'Eliza Documentation',
+  (master_doc, 'Eliza', 'Eliza Documentation',
    author, 'Eliza', 'One line description of project.',
    'Miscellaneous'),
 ]
